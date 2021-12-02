cd8ae4b0bc50f91f5045d3fb7d04949cee0d3ff1
s18015565 <57199591+s18015565@users.noreply.github.com>
2021-12-02 17:55:24 -0600

    Añadidas clases de entidades y cambio del nombre del proyecto

---
+++ Entities/VOCarro.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    class VOCarro
+    {
+
+        public int IdCarro { get; set; }
+        public string Matricula { get; set; }
+        public int Año { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public bool Disponibilidad { get; set; }
+        public string UrlFoto { get; set; }
+
+        public VOCarro(int idCarro, string matricula, int año, string marca, 
+            string modelo, bool disponibilidad, string urlFoto)
+        {
+            IdCarro = idCarro;
+            Matricula = matricula;
+            Año = año;
+            Marca = marca;
+            Modelo = modelo;
+            Disponibilidad = disponibilidad;
+            UrlFoto = urlFoto;
+        }
+
+        public VOCarro(DataRow fila)
+        {
+            IdCarro = (int)fila["IdCarro"];
+            Matricula = (string)fila["Matricula"];
+            Año = (int)fila["Año"];
+            Marca = (string)fila["Marca"];
+            Modelo = (string)fila["Modelo"];
+            Disponibilidad = bool.Parse(fila["Disponibilidad"].ToString());
+            UrlFoto = (string)fila["UrlFoto"];
+        }
+    }
+
+
+    }
+}
